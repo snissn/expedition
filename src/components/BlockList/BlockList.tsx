ed489232f3b164a45ee0fb5495c876c360f7b74f
Mike Seiler <snissn@gmail.com>
2021-01-12 21:20:36 -1000

    change homepage to remove two unnecessary columns - the miner address and the number of uncles

--- src/components/BlockList/BlockList.tsx
+++ src/components/BlockList/BlockList.tsx
@@ -1,4 +1,13 @@
-import { Table, TableBody, TableCell, TableHead, TableRow, Typography, LinearProgress, Tooltip } from "@material-ui/core";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  Typography,
+  LinearProgress,
+  Tooltip
+} from "@material-ui/core";
 import * as React from "react";
 import Link from "@material-ui/core/Link";
 import { hexToDate, hexToNumber, hexToString } from "@etclabscore/eserialize";
@@ -6,7 +15,7 @@ import { Link as RouterLink } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 
 const rightPaddingFix = {
-  paddingRight: "24px",
+  paddingRight: "24px"
 };
 
 function BlockList({ blocks }: any) {
@@ -14,41 +23,60 @@ function BlockList({ blocks }: any) {
   if (!blocks) {
     return null;
   }
-  const sortedBlocks = blocks.sort((a: { number: number }, b: { number: number }) => {
-    return b.number - a.number;
-  });
+  const sortedBlocks = blocks.sort(
+    (a: { number: number }, b: { number: number }) => {
+      return b.number - a.number;
+    }
+  );
   return (
     <div style={{ width: "100%", overflowX: "auto" }}>
       <Table>
         <TableHead>
           <TableRow>
-            <TableCell><Typography>{t("Author")}</Typography></TableCell>
-            <TableCell><Typography>{t("Block Number")}</Typography></TableCell>
-            <TableCell><Typography>{t("Timestamp")}</Typography></TableCell>
-            <TableCell><Typography>{t("#Txs")}</Typography></TableCell>
-            <TableCell><Typography>{t("Gas Usage")}</Typography></TableCell>
-            <TableCell><Typography>{t("Gas Limit")}</Typography></TableCell>
-            <TableCell><Typography>{t("Uncles")}</Typography></TableCell>
-            <TableCell><Typography>{t("Hash")}</Typography></TableCell>
+            <TableCell>
+              <Typography>{t("Block Number")}</Typography>
+            </TableCell>
+            <TableCell>
+              <Typography>{t("Timestamp")}</Typography>
+            </TableCell>
+            <TableCell>
+              <Typography>{t("#Txs")}</Typography>
+            </TableCell>
+            <TableCell>
+              <Typography>{t("Gas Usage")}</Typography>
+            </TableCell>
+            <TableCell>
+              <Typography>{t("Gas Limit")}</Typography>
+            </TableCell>
+            <TableCell>
+              <Typography>{t("Hash")}</Typography>
+            </TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {sortedBlocks.map((b: any, index: number) => {
-            const filledPercent = (hexToNumber(b.gasUsed) / hexToNumber(b.gasLimit)) * 100;
+            const filledPercent =
+              (hexToNumber(b.gasUsed) / hexToNumber(b.gasLimit)) * 100;
 
             // Shorten hash views by concatenating first and last 4 chars.
-            const blockHashShort = b.hash.substring(2, 6) + '—' + b.hash.substring(b.hash.length - 5, b.hash.length - 1);
-            const authorHashShort = b.miner.substring(2, 6) + '—' + b.miner.substring(b.miner.length - 5, b.miner.length - 1);
+            const blockHashShort =
+              b.hash.substring(2, 6) +
+              "—" +
+              b.hash.substring(b.hash.length - 5, b.hash.length - 1);
+            const authorHashShort =
+              b.miner.substring(2, 6) +
+              "—" +
+              b.miner.substring(b.miner.length - 5, b.miner.length - 1);
 
             // Colorize left border derived from author credit account.
             const authorHashStyle = {
-              borderLeft: `1em solid #${b.miner.substring(2, 8)}`,
+              borderLeft: `1em solid #${b.miner.substring(2, 8)}`
             };
 
             // Tally transactions which create contracts vs transactions with addresses.
             var txTypes = {
               create: 0,
-              transact: 0,
+              transact: 0
             };
 
             for (var i = 0; i < b.transactions.length; i++) {
@@ -60,39 +88,56 @@ function BlockList({ blocks }: any) {
             }
 
             // Calculate difference of block timestamp from that of parent.
-            const timeDifferenceFromParent = (index === sortedBlocks.length - 1) ? 0 : hexToNumber(b.timestamp) - hexToNumber(sortedBlocks[index + 1].timestamp);
+            const timeDifferenceFromParent =
+              index === sortedBlocks.length - 1
+                ? 0
+                : hexToNumber(b.timestamp) -
+                  hexToNumber(sortedBlocks[index + 1].timestamp);
 
             return (
               <TableRow key={b.number} style={authorHashStyle}>
-                <TableCell style={rightPaddingFix}>
-                  <Typography>
-                    <Link
-                      component={({ className, children }: { children: any, className: string }) => (
-                        <RouterLink className={className} to={`/address/${b.miner}`} >
-                          {children}
-                        </RouterLink>
-                      )}>
-                      {authorHashShort}
-                    </Link>
-                    &nbsp;<sup>{hexToString(b.extraData).substring(0, 20)}</sup>
-                  </Typography>
-                </TableCell>
                 <TableCell component="th" scope="row">
                   <Link
-                    component={({ className, children }: { children: any, className: string }) => (
-                      <RouterLink className={className} to={`/block/${b.hash}`} >
+                    component={({
+                      className,
+                      children
+                    }: {
+                      children: any;
+                      className: string;
+                    }) => (
+                      <RouterLink className={className} to={`/block/${b.hash}`}>
                         {children}
                       </RouterLink>
-                    )}>
+                    )}
+                  >
                     {parseInt(b.number, 16)}
                   </Link>
                 </TableCell>
                 <TableCell style={rightPaddingFix}>
-                  <Typography>{t("Timestamp Date", { date: hexToDate(b.timestamp) })}&nbsp;<sub>({timeDifferenceFromParent > 0 ? `+${timeDifferenceFromParent}` : `-${timeDifferenceFromParent}`}s)</sub></Typography>
+                  <Typography>
+                    {t("Timestamp Date", { date: hexToDate(b.timestamp) })}
+                    &nbsp;
+                    <sub>
+                      (
+                      {timeDifferenceFromParent > 0
+                        ? `+${timeDifferenceFromParent}`
+                        : `-${timeDifferenceFromParent}`}
+                      s)
+                    </sub>
+                  </Typography>
                 </TableCell>
                 <TableCell style={rightPaddingFix}>
-                  <Tooltip title={t("Create Transactions", {count: txTypes.create}) as string} placement="top">
-                    <Typography variant="caption" color="textSecondary">{txTypes.create === 0 ? "" : txTypes.create}</Typography>
+                  <Tooltip
+                    title={
+                      t("Create Transactions", {
+                        count: txTypes.create
+                      }) as string
+                    }
+                    placement="top"
+                  >
+                    <Typography variant="caption" color="textSecondary">
+                      {txTypes.create === 0 ? "" : txTypes.create}
+                    </Typography>
                   </Tooltip>
                   <Typography>{txTypes.transact}</Typography>
                 </TableCell>
@@ -102,16 +147,20 @@ function BlockList({ blocks }: any) {
                 <TableCell>
                   <Typography>{hexToNumber(b.gasLimit)}</Typography>
                 </TableCell>
-                <TableCell>
-                  <Typography>{b.uncles.length === 0 ? "" : b.uncles.length}</Typography>
-                </TableCell>
                 <TableCell style={rightPaddingFix}>
                   <Link
-                    component={({ className, children }: { children: any, className: string }) => (
-                      <RouterLink className={className} to={`/block/${b.hash}`} >
+                    component={({
+                      className,
+                      children
+                    }: {
+                      children: any;
+                      className: string;
+                    }) => (
+                      <RouterLink className={className} to={`/block/${b.hash}`}>
                         {children}
                       </RouterLink>
-                    )}>
+                    )}
+                  >
                     {blockHashShort}
                   </Link>
                 </TableCell>
@@ -121,7 +170,6 @@ function BlockList({ blocks }: any) {
         </TableBody>
       </Table>
     </div>
-
   );
 }
 
